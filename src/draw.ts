2f2310cdb9932bbcaa6b5632ed8b8e3442bd2791
RodrigoDornelles <rodrigo@dornelles.me>
2023-11-05 17:27:52 -0300

    feat: support multi touch

--- src/draw.ts
+++ src/draw.ts
@@ -1,4 +1,23 @@
-import { ObjectGpz } from "./interface";
+import { ObjectGpz, Vector2d } from "./interface";
+
+function nestFinger(center: Vector2d, fingers: Array<Vector2d>): {dis: number, pos: Vector2d} {
+    if (fingers.length == 0) {
+        return {dis: 0, pos: center}
+    }
+
+    let shortestDistance : number = Number.MAX_SAFE_INTEGER
+    let closestFinger: Vector2d = fingers[0]
+
+    fingers.forEach(finger => {
+        const distance = Math.sqrt((finger.x - center.x) ** 2 + (finger.y - center.y) ** 2)
+        if (distance < shortestDistance) {
+            shortestDistance = distance
+            closestFinger = finger
+        }
+    })
+
+    return {dis: shortestDistance, pos: closestFinger}
+}
 
 function drawCircle(ctx: CanvasRenderingContext2D, fill: string, x: number, y: number, r: number) {
     ctx.beginPath()
@@ -17,42 +36,33 @@ const draw = {
         const radius: number = 25;
         let color: string = '#88888880'
 
-        if (self.fingers.length) {
-            const distance = Math.sqrt((self.fingers[0].x - centerX) ** 2 + (self.fingers[0].y - centerY) ** 2)
+        self.fingers.forEach(finger => {
+            const distance = Math.sqrt((finger.x - centerX) ** 2 + (finger.y - centerY) ** 2)
             if (distance < radius) {
                 color = 'red'
             }
-        }
+        })
 
         self.ctx2d.clearRect(0, 0, self.canvas.width, self.canvas.height)
         drawCircle(self.ctx2d, color, centerX, centerY, radius)
     },
     Joy(self: ObjectGpz) {
-        const centerX: number = self.canvas.width / 2
-        const centerY: number = self.canvas.height / 2
+        const center: Vector2d = {x: self.canvas.width / 2, y: self.canvas.height / 2}
         const radius: number = 50
         const radius2: number = radius/3
-        const percentage: number = 1.5
+        const percentage: number = 1.5    
+        const stick = nestFinger(center, self.fingers)
     
-        self.ctx2d.clearRect(0, 0, self.canvas.width, self.canvas.height)
-    
-        const posX: number = self.fingers[0]?.x ?? centerX
-        const posY: number = self.fingers[0]?.y ?? centerY
-        const distance = Math.sqrt((posX - centerX) ** 2 + (posY - centerY) ** 2)
-    
-        let secondCircleX = posX
-        let secondCircleY = posY
-    
-        if ((distance / percentage) > (radius - radius2)) {
-            const angle = Math.atan2(posY - centerY, posX - centerX)
-            secondCircleX = centerX + (radius - radius2) * percentage * Math.cos(angle)
-            secondCircleY = centerY + (radius - radius2) * percentage * Math.sin(angle)
+        if ((stick.dis / percentage) > (radius - radius2)) {
+            const angle = Math.atan2(stick.pos.y - center.y, stick.pos.x - center.x)
+            stick.pos.x = center.x + (radius - radius2) * percentage * Math.cos(angle)
+            stick.pos.y = center.y + (radius - radius2) * percentage * Math.sin(angle)
         }
     
-        drawCircle(self.ctx2d, '#aaaaaa80', centerX, centerY, radius)
-        drawCircle(self.ctx2d, '#88888880', secondCircleX, secondCircleY, radius2)
+        self.ctx2d.clearRect(0, 0, self.canvas.width, self.canvas.height)
+        drawCircle(self.ctx2d, '#aaaaaa80', center.x, center.y, radius)
+        drawCircle(self.ctx2d, '#88888880', stick.pos.x, stick.pos.y, radius2)
     }
 }
 
 export { draw }
-
