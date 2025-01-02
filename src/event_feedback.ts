f97061f1c67d8691374f86efc8d5882aab214b14
RodrigoDornelles <rodrigo@dornelles.me>
2025-01-01 23:22:12 -0300

    fix: Blocked call to navigator.vibrate #38

--- src/event_feedback.ts
+++ src/event_feedback.ts
@@ -1,9 +1,9 @@
 import { ObjectGpz } from "./interface";
 
-function vibration(self: ObjectGpz, device: Navigator) {
+function vibration(self: ObjectGpz, device: Navigator, first_interact: {click: boolean}) {
     const find = (state, index) => state && self.stateOld[index] == false
     const action = self.stateNew.findIndex(find)
-    if (action !== -1) {
+    if (action !== -1 && first_interact.click) {
         const vibration = self.canvas.dataset.gpzVibrate ?? '200'
         const animation = vibration.split(' ').map(intensity => Number(intensity))
         device.vibrate(animation)
@@ -12,9 +12,17 @@ function vibration(self: ObjectGpz, device: Navigator) {
 
 function InstallEventFeedback(device: Window, pads: Array<ObjectGpz>)
 {
+    let first_interact = {
+        click: false
+    }
+
+    device.addEventListener('click', () => {
+        first_interact.click = true
+    })
+
     pads.forEach(self => {
         if ('navigator' in device && 'vibrate' in device.navigator) {
-            self.chain.push(pad => vibration(pad, device.navigator))
+            self.chain.push(pad => vibration(pad, device.navigator, first_interact))
         }
     })
 }
